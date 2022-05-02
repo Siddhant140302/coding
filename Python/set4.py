2ee87b89ae81887f921d0dac00bb7bd1108bd86a
Siddhant140302 <siddhantbhadoriya12@gmail.com>
2022-05-02 22:09:35 +0530

    new

---
+++ Python/set4.py
@@ -0,0 +1,34 @@
+'''f1=input("Enter fruit number 1: ")       #Q1      
+f2=input("Enter fruit number 2: ")
+f3=input("Enter fruit number 3: ")
+f4=input("Enter fruit number 4: ")
+f5=input("Enter fruit number 5: ")
+f6=input("Enter fruit number 6: ")
+f7=input("Enter fruit number 7: ")
+myFruitList=[f1,f2,f3,f4,f5,f6,f7]
+print(myFruitList)'''
+
+#Q2
+'''m1=int(input("Enter marks of student 1: "))        
+m2=int(input("Enter marks of student 2: "))
+m3=int(input("Enter marks of student 3: "))
+m4=int(input("Enter marks of student 4: "))
+m5=int(input("Enter marks of student 5: "))
+m6=int(input("Enter marks of student 6: "))
+mylist=[m1,m2,m3,m4,m5,m6]
+mylist.sort()
+print(mylist)'''
+
+#Q3
+'''a=(2,4,5,6,7)
+a[0]=45'''
+
+#Q4
+'''a=[2,4,40,23]
+print(a[0]+a[1]+a[2]+a[3])
+print(sum(a))'''
+
+#Q5
+a=(7,0,8,0,0,9)
+anew=a.count(0)
+print("The number of zeros are: ",anew)
