215cfc59b6461d76f55514a47f5ca1142e951762
Siddhant140302 <siddhantbhadoriya12@gmail.com>
2022-05-02 23:17:42 +0530

    init

---
+++ Python/set5.py
@@ -0,0 +1,23 @@
+#Q1
+'''mydict={
+    "Pankha": "Fan",
+    "Dabba": "Box",
+    "Vastu": "Object"
+}
+print("Options are: ",mydict.keys())
+a=input("Enter the Hindi Word\n")
+print("The meaning of your word is:",mydict.get(a))'''
+
+#Q2
+num1=int(input("Enter number 1:"))
+num2=int(input("Enter number 2:"))
+num3=int(input("Enter number 3:"))
+num4=int(input("Enter number 4:"))
+num5=int(input("Enter number 5:"))
+num6=int(input("Enter number 6:"))
+num7=int(input("Enter number 7:"))
+num8=int(input("Enter number 8:"))
+s={num1,num2,num3,num4,num5,num6,num7,num8}
+print(s)
+
+#Q3
