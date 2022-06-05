4cc94cb09cce135444306b73c74243a725c4a325
Siddhant140302 <siddhantbhadoriya12@gmail.com>
2022-06-05 16:27:47 +0530

    init

--- Python/sudokugame.py
+++ Python/sudokugame.py
@@ -10,66 +10,29 @@ response = requests.get("https://sugoku.herokuapp.com/board?difficulty=easy")
 grid = response.json()['board']
 grid_original = [[grid[x][y] for y in range(len(grid[0]))] for x in range(len(grid))]
 
-
-def isEmpty(num):
-    if num == 0:
-        return True
-    return False
-
-def isValid(position, num):
-     #Check for Column, row and sub-grid
-    
-    #Checking row
-    for i in range(0, len(grid[0])):
-        if(grid[position[0]][i] == num):
-            return False
-    
-    #Checking column
-    for i in range(0, len(grid[0])):
-        if(grid[i][position[1]] == num):
-            return False
-    
-    #Check sub-grid  
-    x = position[0]//3*3
-    y = position[1]//3*3
-    #Gives us the box number
-    
-    for i in range(0,3):
-        for j in range(0,3):
-            if(grid[x+i][y+j]== num):
-                return False
-    return True
-
-
-solved = 0
-def sudoku_solver(win):
+def insert(win, position):
+    i,j = position[1], position[0]
     myfont = pygame.font.SysFont('Comic Sans MS', 35)
-    for i in range(0,len(grid[0])):
-        for j in range(0, len(grid[0])):
-            if(isEmpty(grid[i][j])): 
-                for k in range(1,10):
-                    if isValid((i,j), k):                   
-                        grid[i][j] = k
-                        pygame.draw.rect(win, background_color, ((j+1)*50 + buffer, (i+1)*50+ buffer,50 -2*buffer , 50 - 2*buffer))
-                        value = myfont.render(str(k), True, (0,0,0))
-                        win.blit(value, ((j+1)*50 +15,(i+1)*50))
-                        pygame.display.update()
-                        pygame.time.delay(25)
-                        
-                        sudoku_solver(win)
-                        
-                        #Exit condition
-                        global solved
-                        if(solved == 1):
-                            return
-                        
-                        #if sudoku_solver returns, there's a mismatch
-                        grid[i][j] = 0
-                        pygame.draw.rect(win, background_color, ((j+1)*50 + buffer, (i+1)*50+ buffer,50 -2*buffer , 50 - 2*buffer))
-                        pygame.display.update()
-                        #pygame.time.delay(50)
-                return               
-    solved = 1
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.KEYDOWN:
+                if(grid_original[i-1][j-1] != 0):
+                    return
+                if(event.key == 48): #checking with 0
+                    grid[i-1][j-1] = event.key - 48
+                    pygame.draw.rect(win, background_color, (position[0]*50 + buffer, position[1]*50+ buffer,50 -2*buffer , 50 - 2*buffer))
+                    pygame.display.update()
+                    return
+                if(0 < event.key - 48 <10):  #We are checking for valid input
+                    pygame.draw.rect(win, background_color, (position[0]*50 + buffer, position[1]*50+ buffer,50 -2*buffer , 50 - 2*buffer))
+                    value = myfont.render(str(event.key-48), True, (0,0,0))
+                    win.blit(value, (position[0]*50 +15, position[1]*50))
+                    grid[i-1][j-1] = event.key - 48
+                    pygame.display.update()
+                    return
+                return
         
 
 
@@ -96,9 +59,12 @@ def main():
                 win.blit(value, ((j+1)*50 + 15, (i+1)*50 ))
     pygame.display.update()
             
-    sudoku_solver(win)
+        
     while True: 
         for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                pos = pygame.mouse.get_pos()
+                insert(win, (pos[0]//50, pos[1]//50))
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
