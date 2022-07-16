b96fe3cbd78fa0a8bdbaa6020ee9149dde6a725f
Siddhant140302 <siddhantbhadoriya12@gmail.com>
2022-07-17 02:02:26 +0530

    init

--- Python/keypress.py
+++ Python/keypress.py
@@ -1,17 +1,20 @@
 import turtle
-
-
-import turtle
+from turtle import Screen
 turtle.setup(400,500)
-wn=turtle.screen()
-wn.title("Handling keypress! in Python!")
-wn.bgcolor("blue")
+window=turtle.Screen()
+window.title("Handling keypress! in Python!")
+window.bgcolor("blue")
 t=turtle.Turtle()
 t.color("black")
 t.shape("turtle")
 def move():
-    t.forward(2)
+    t.forward(100)
 def turn_left():
     t.left(90)
 def exit():
-    wn.bye()
+    window.bye()
+window.onkey(move, 'space')
+window.onkey(exit, 'q')
+
+window.listen()
+window.mainloop()
