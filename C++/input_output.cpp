34d696813b843fa31a9ebbf2d832fbf7093607f2
Siddhant140302 <siddhantbhadoriya12@gmail.com>
2022-08-08 22:21:32 +0530

    init

---
+++ C++/input_output.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout<<"Enter the Value of num1:";
+    cin>>num1;
+   
+    cout<<"Enter the Value of num2:";
+    cin>>num2;
+   
+    cout<<"The Sum is "<<num1+num2;
+    return 0;
+
+}
